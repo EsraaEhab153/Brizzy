e5e510b4c1d84b7de79ae934ed200c29395dce97
esraa_ehab <esraaehab530@gmail.com>
2026-03-05 03:38:02 +0200

    finished weather Repository

---
+++ app/src/main/java/com/example/brizzy/data/weather/WeatherRepository.kt
@@ -0,0 +1,9 @@
+package com.example.brizzy.data.weather
+
+import com.example.brizzy.data.weather.model.WeatherResponse
+import kotlinx.coroutines.flow.Flow
+
+interface WeatherRepository {
+
+    suspend fun getWeather(lat: Double, lon: Double): Flow<WeatherResponse>
+}
